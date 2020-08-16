1ce0535f30a399ce42d96c30bdb759a31d37395a
LM&T <tbbragg83@gmail.com>
2020-08-16 00:36:51 -0500

    added artist and attendee models/adjusted schema/changed db in developement config.json

---
+++ models/artist.js
@@ -0,0 +1,35 @@
+const { sequelize } = require(".");
+
+module.exports = function(sequelize, DataTypes) {
+  const Artist = sequelize.define("Artist", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    song: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    videoID: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Artist.associate = function(models) {
+    Artist.belongsTo(models.Attendee, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Artist;
+};
---
+++ models/attendee.js
@@ -0,0 +1,26 @@
+const { sequelize } = require(".");
+
+module.exports = function(sequelize, DataTypes) {
+  const Attendee = sequelize.define("Attendee", {
+    name: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  });
+
+  Attendee.associate = function(models) {
+    Attendee.belongsTo(models.User);
+    Attendee.hasMany(models.Artist, {
+      onDelete: "cascade"
+    });
+  };
+
+//   Attendee.associate = function(models) {
+//     Attendee.hasMany(models.Post, {
+//       onDelete: "cascade"
+//     });
+//   };
+
+  return Attendee;
+};
