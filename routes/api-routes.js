589f4f214b8df9cca01a9cbaa8a8f85f697a2369
LM&T <tbbragg83@gmail.com>
2020-08-18 23:09:35 -0500

    added api route for index.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 // Requiring our models and passport as we've configured it
 const db = require("../models");
 const passport = require("../config/passport");
+const { Router } = require("express");
 
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
@@ -51,3 +52,8 @@ module.exports = function(app) {
     }
   });
 };
+
+Router.get("/", (req, res) => {
+  res.send("index.js");
+});
+
