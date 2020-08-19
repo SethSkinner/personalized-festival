6b5e0e562aea1c467a5eeee218a0982fadc3fdb7
seth skinner <sethwskinner@gmail.com>
2020-08-19 00:05:00 -0500

    fixed passport

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,9 +8,9 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.renders("/members");
     }
-    res.sendFile(path.join(__dirname, "../views/layouts/signup.handlebars"));
+    res.render('signup');
   });
 
   app.get("/login", (req, res) => {
@@ -18,12 +18,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../views/layouts/login.handlebars"));
+    res.render('login');
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/layouts/members.handlebars"));
+    res.render('index');
   });
 };
