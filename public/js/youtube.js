fd6144298cbc48de46fd264e1b1669863b8ea534
seth skinner <sethwskinner@gmail.com>
2020-08-20 22:44:40 -0500

    adding youtube to the page

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,12 +1,12 @@
 $(document).ready(() => {
   //seth1
-  // const API_KEY = "AIzaSyB8RKx81I_wLM5TZa2jNR6Jd8j04yYypsI";
+   const API_KEY = "AIzaSyB8RKx81I_wLM5TZa2jNR6Jd8j04yYypsI";
   //Thomas1
   // const API_KEY = "AIzaSyBqnUPy6o1c-V4MGOw_1GcCueLKFJOSo5s";
   //Thomas2
   // const API_KEY = "AIzaSyALxc9zxI-pJp33r-9awOLo4AWY3wYydrQ";
   //Maria
-  const API_KEY = "AIzaSyC6dPvMs2eQxvcoGDRFHvgAhlQx0TqUNCA";
+  //const API_KEY = "AIzaSyC6dPvMs2eQxvcoGDRFHvgAhlQx0TqUNCA";
 
   // eslint-disable-next-line prettier/prettier
   $("#form1").submit((event) => {
@@ -36,19 +36,9 @@ $(document).ready(() => {
     $.post("/api/songs", songData);
   }
 
-  function appendVideo(data) {
-    // eslint-disable-next-line prettier/prettier
-    data.items.forEach((item) => {
-      video = `
-            <iframe width='420' height='315' src='http://www.youtube.com/embed/${item.id.videoId}' frameboarder='0' allowfullscreen></iframe>`;
 
-      $("#videos").append(video);
-    });
-  }
 
   function videoSearch(API_KEY, search, maxResults) {
-    $("#videos").empty();
-
     $.get(
       "https://www.googleapis.com/youtube/v3/search?key=" +
         API_KEY +
@@ -65,8 +55,128 @@ $(document).ready(() => {
           // eslint-disable-next-line prettier/prettier
           name: data.items[0].snippet.title,
         });
-        appendVideo(data);
       }
     );
   }
 });
+
+var YouTubePlayer = {
+  current: 0,
+  player: null,
+  /**
+   * Tracks ids here...
+   */
+  videos: [
+      'cpbbuaIA3Ds',
+  'OtcdGkGusU0'
+  ],
+  currentlyPlaying:function(){
+      console.info('Current Track id', YouTubePlayer.videos[YouTubePlayer.current]);
+      return YouTubePlayer.videos[YouTubePlayer.current];
+  },
+  playNext: function () {
+      YouTubePlayer.increaseTrack()
+      debugger
+      if (YouTubePlayer.player) {
+          YouTubePlayer.player.loadVideoById(YouTubePlayer.currentlyPlaying());
+      } else {
+          alert('Please Wait! Player is loading');
+      }
+  },
+  playPrevious: function () {
+      YouTubePlayer.decreaseTrack()
+      if (YouTubePlayer.player) {
+          YouTubePlayer.player.loadVideoById(YouTubePlayer.currentlyPlaying());
+      } else {
+          alert('Please Wait! Player is loading');
+      }
+
+  },
+  increaseTrack: function () {
+      YouTubePlayer.current = YouTubePlayer.current + 1;
+      if (YouTubePlayer.current >= YouTubePlayer.videos.length) {
+          YouTubePlayer.current = 0;
+      }
+  },
+  decreaseTrack: function () {
+      YouTubePlayer.current = Math.max(YouTubePlayer.current - 1, 0);
+  },
+  onReady: function (event) {
+      event.target.loadVideoById(YouTubePlayer.currentlyPlaying());
+  },
+  onStateChange: function (event) {
+      if (event.data == YT.PlayerState.ENDED) {
+          YouTubePlayer.playNext();
+      }
+  }
+}
+function onYouTubeIframeAPIReady() {
+  YouTubePlayer.player = new YT.Player('youtube', {
+      height: '350',
+      width: '425',
+      events: {
+          'onReady': YouTubePlayer.onReady,
+          'onStateChange': YouTubePlayer.onStateChange
+      }
+  });
+}
+var YouTubePlayer = {
+  current: 0,
+  player: null,
+  /**
+   * Tracks ids here...
+   */
+  videos: [
+      'cpbbuaIA3Ds',
+  'OtcdGkGusU0'
+  ],
+  currentlyPlaying:function(){
+      console.info('Current Track id', YouTubePlayer.videos[YouTubePlayer.current]);
+      return YouTubePlayer.videos[YouTubePlayer.current];
+  },
+  playNext: function () {
+      YouTubePlayer.increaseTrack()
+      debugger
+      if (YouTubePlayer.player) {
+          YouTubePlayer.player.loadVideoById(YouTubePlayer.currentlyPlaying());
+      } else {
+          alert('Please Wait! Player is loading');
+      }
+  },
+  playPrevious: function () {
+      YouTubePlayer.decreaseTrack()
+      if (YouTubePlayer.player) {
+          YouTubePlayer.player.loadVideoById(YouTubePlayer.currentlyPlaying());
+      } else {
+          alert('Please Wait! Player is loading');
+      }
+
+  },
+  increaseTrack: function () {
+      YouTubePlayer.current = YouTubePlayer.current + 1;
+      if (YouTubePlayer.current >= YouTubePlayer.videos.length) {
+          YouTubePlayer.current = 0;
+      }
+  },
+  decreaseTrack: function () {
+      YouTubePlayer.current = Math.max(YouTubePlayer.current - 1, 0);
+  },
+  onReady: function (event) {
+      event.target.loadVideoById(YouTubePlayer.currentlyPlaying());
+  },
+  onStateChange: function (event) {
+      if (event.data == YT.PlayerState.ENDED) {
+          YouTubePlayer.playNext();
+      }
+  }
+}
+function onYouTubeIframeAPIReady() {
+  YouTubePlayer.player = new YT.Player('youtube', {
+      height: '350',
+      width: '425',
+      events: {
+          'onReady': YouTubePlayer.onReady,
+          'onStateChange': YouTubePlayer.onStateChange
+      }
+  });
+}
